2470ae5209821b108ed14fd10be6034e8060ca8a
Patrick Halton <phalton04@fullsail.edu>
2016-01-05 20:46:48 -0500

    push

---
+++ templates/company.php
@@ -0,0 +1,65 @@
+<div id="jumbo" class="jumbotron">
+
+    <div class="row text-center">
+        <a id="back" class="glyphicon glyphicon-arrow-left" href="/"></a>
+
+        <h1 id="float">Patrick Halton Enterprises</h1>
+
+
+        <div id="half" class="col-sm-6 text-right">
+            <a id="company" href="/index.php/company">My Resume</a>
+
+        </div>
+
+        <div id="half1" class="col-sm-6 text-left">
+            <a id="services" href="/index.php/services">My Portfolio</a>
+        </div>
+    </div>
+</div>
+
+<div class="row">
+
+    <div class="col-md-6 text-center">
+        <h3 id="ff">Patrick Halton</h3>
+        <img class="prof" src="../img/Pat_pro.png" alt="pat">
+        <h4 class="text-center">Full Stack Developer</h4>
+        <h5 id="move" class="text-center">Main Skills:</h5>
+        <ul class="skills">
+            <li>Web Design</li>
+            <li>Web Development</li>
+            <li>Content Management</li>
+            <li>Social Media Marketing</li>
+        </ul>
+    </div>
+
+    <div id="morem" class="col-md-6">
+
+        <h1 id="ab">About Me</h1><hr>
+
+                <p class="big">I am a motivated and passionate web developer and designer.I also love providing great content to websites in order to give a website its maximum potential. I am familiar in javascript, nodeJS, html, css, foundation, photoshop, illustrator,python, codeigniter, mySQL but I love php. Always eager to learn to learn new technologies and problem solve.</p>
+                <p class="big">I also have a great passion in the business side of websites. By myself, I was able to make fully functional web applications for clients before I finished my degree program. I love talking with clients and have very good verbal communication skills that seek out exactly what a client needs and enough information from the client to help me make the best desired product.</p>
+
+
+</div>
+
+<div id="down" class="row">
+
+</div>
+
+
+<h1 id="h" class="text-center">Resume</h1><hr>
+
+<div class="col-lg-12 text-center">
+
+    <a id="biggg" href="../img/Patrick_Halton_WrittenResume.doc" target='_blank'
+       title="Click here to open a Word document">
+        Click here to open Resume</a>
+
+</div>
+
+
+</div>
+
+<div id="contact2" class="text-center"><a id="cont" href= "mailto:phalton04@fullsail.edu.com">Contact Me</a></div>
+
+
---
+++ templates/footer.php
@@ -0,0 +1,6 @@
+<footer class="col-md-12 text-center"><h1>Patrick Halton Enterprises&copy;</h1></footer>
+
+</body>
+
+
+</html>
